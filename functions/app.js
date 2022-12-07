cec3237f3934a4def08094b423c2aab0ad6c4d15
Tanmay <tanmaynl37@gmail.com>
2022-12-07 11:51:27 +0530

    changes

--- functions/app.js
+++ functions/app.js
@@ -1,4 +1,4 @@
-
+//rest parameter allows us to pass an indefinite number of arguments as an array
 function greet(message,...names){
     console.log(message + " " + 'everyone');
     names.forEach(name => console.log(message + " " + name));
