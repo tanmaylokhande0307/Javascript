508c65aa338fdf04822e2cb95096ae702dd679ea
Tanmay <tanmaynl37@gmail.com>
2022-12-09 11:38:53 +0530

    c

--- closures/index.js
+++ closures/index.js
@@ -1,24 +1,25 @@
-// function outer(){
-//     function inner(){
-//         console.log(a);
-//     }
-//     let a = 1;
-//     return inner;
-// }
-
-// const inn = outer();
-
-// inn();
-
-
-for(var i = 0; i< 3; i++) {
-   function print(c){
-        setTimeout(() => {
-            console.log(c);
-        },i*1000)
-   }
-   print(i);
+function outer(){
+    
+    var a = 1,b = 2;
+    return function inner(){
+        console.log(a);
+    }
+    
 }
+
+const inn = outer();
+
+inn();
+
+
+// for(var i = 0; i< 3; i++) {
+//    function print(c){
+//         setTimeout(() => {
+//             console.log(c);
+//         },i*1000)
+//    }
+//    print(i);
+// }
 
 
 // function counter(){
