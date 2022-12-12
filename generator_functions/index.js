d5de92708d2ef9ce276f204aea78eff17e47d192
Tanmay <tanmaynl37@gmail.com>
2022-12-12 09:39:12 +0530

    update

--- generator_functions/index.js
+++ generator_functions/index.js
@@ -1,10 +1,25 @@
 function *timeStampGenerator(){
-    console.log(Date.now().toString());
-    yield;
-    console.log("execution continues");
+   
+    var ts = Date.now();
+    console.log('originalTimestamp ', ts);
+
+    yield ts;    
+    console.log('yield ',yield);
+
+    console.log('waiting');
+   
+    var additionalTime = yield;
+    console.log('additional time ',additionalTime);
+   
+    if(additionalTime){
+        ts = ts + additionalTime;
+    }
+   
+    console.log('updated time ',ts)
 }
 
-const it = timeStampGenerator();
-it.next();
-it.next();
-
+    const it = timeStampGenerator();
+    const originalTimestamp = it.next();
+    console.log(originalTimestamp);
+    it.next();
+    it.next(1000*6)
