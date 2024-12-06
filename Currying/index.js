79e13c240fb06aaa7b2e29bcb35546fb20a29c1b
debian <tl@zasmlabs.com>
2024-12-06 22:13:17 +0530

    curried sum

--- Currying/index.js
+++ Currying/index.js
@@ -1,3 +1,7 @@
-const f = (a) => (b) => console.log(a,b)
+const f = (a) => (b) => console.log(a, b);
 
-console.log(f(2)(3))
+// console.log(f(2)(3))
+
+const sum = (a) => (b) => (c) => a + b + c;
+
+console.log(sum(1)(3)(3))
