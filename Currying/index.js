8c90acf9deb20653c83f47e2e22151211a47b6ed
debian <tl@zasmlabs.com>
2024-12-06 22:35:33 +0530

    curried evaluate

--- Currying/index.js
+++ Currying/index.js
@@ -4,4 +4,17 @@ const f = (a) => (b) => console.log(a, b);
 
 const sum = (a) => (b) => (c) => a + b + c;
 
-console.log(sum(1)(3)(3))
+// console.log(sum(1)(3)(3))
+
+const operationMapping = {
+  sum: (x, y) => x + y,
+  subtraction: (x, y) => x - y,
+  multiply: (x, y) => x * y,
+  divide: (x, y) => x / y,
+};
+
+const evaluate = (operation) => (x) => (y) => {
+    return operationMapping[operation](x,y)
+};
+
+console.log(evaluate("sum")(1)(2));
