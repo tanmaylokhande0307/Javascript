b5fd96576b9d0647b50eefba6359ff5252064ff7
debian <tl@zasmlabs.com>
2025-01-28 22:41:09 +0530

    added data

---
+++ graphQl_expt/src/data/product.js
@@ -0,0 +1,24 @@
+export const products = [
+    {
+        id:"1",
+        title: "Laptop",
+        category: "Electronics",
+        price: 1000,
+        inStock: true
+    },
+    {
+        id:"2",
+        title: "Coffee",
+        category: "Food",
+        price: 10,
+        inStock: true
+    },
+    {
+        id:"3",
+        title: "Shoes",
+        category: "Sports",
+        price: 50,
+        inStock: false
+    }
+]
+
