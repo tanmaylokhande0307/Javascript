eadfe460a8fb386374fa4f6ed563b8a05664cf04
debian <tl@zasmlabs.com>
2025-01-28 23:04:27 +0530

    added a resolver

--- graphQl_expt/src/graphql/resolvers.js
+++ graphQl_expt/src/graphql/resolvers.js
@@ -3,5 +3,6 @@ import { products } from "../data/product.js";
 export const resolvers = {
   Query: {
     products: () => products,
+    product: (_, { id }) => products.find((product) => product.id === id),
   },
 };
