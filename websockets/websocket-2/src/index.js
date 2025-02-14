174a16037cd237fc844cc7dda7a4f47b001cb7dd
debian <tl@zasmlabs.com>
2025-02-14 23:59:38 +0530

    used express server

--- websockets/websocket-2/src/index.js
+++ websockets/websocket-2/src/index.js
@@ -1,21 +1,22 @@
 import { WebSocketServer } from "ws";
 import WebSocket from "ws";
-import http from "http"
+import http from "http";
+import express from "express";
 
-const server = http.createServer((req,res)=>{})
+const app = express();
+const httpserver = app.listen(8080, () => {
+  console.log("server is listening on port 8080 ");
+});
 
-const wss = new WebSocketServer({ server });
+const server = http.createServer((req, res) => {});
+
+const wss = new WebSocketServer({ server: httpserver });
 
 wss.on("connection", (ws) => {
   ws.on("message", (data) => {
     const res = data;
-    console.log(JSON.parse(res.toString()))
-});
+    console.log(JSON.parse(res.toString()));
+  });
 
   console.log("connected");
 });
-
-
-server.listen(8080,()=>{
-    console.log("server is listening on port 8080 ")
-})
